35f36c49d750927cf0910903d31aaefdb1408418
Peter Baranec <peter.baranec.devtech@gmail.com>
2024-03-27 11:59:02 +0100

    display crypto

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// Fetch image from unsplash
 fetch(
   "https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature"
 )
@@ -9,3 +10,14 @@ fetch(
   .catch((err) => {
     document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMTEwMjl8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MjI4NDE2NzA&ixlib=rb-1.2.1&q=80&w=1080)`;
   });
+
+//Fetch crypto from coingecko
+fetch("https://api.coingecko.com/api/v3/coins/bitcoin")
+  .then((res) => res.json())
+  .then((data) => {
+    document.getElementById("crypto-top").innerHTML = `
+    <img src="${data.image.small}" alt="crypto icon">
+    <span>${data.name}</span>
+    `;
+  })
+  .catch((err) => console.error(err));
