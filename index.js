40c56b146b645e07e433f26badcefa354ff072bc
Peter Baranec <peter.baranec.devtech@gmail.com>
2024-03-27 13:14:02 +0100

    get weather information

--- index.js
+++ index.js
@@ -38,3 +38,18 @@ function getCurrentTime() {
 }
 
 setInterval(getCurrentTime, 1000);
+
+navigator.geolocation.getCurrentPosition((position) => {
+  fetch(
+    `https://apis.scrimba.com/openweathermap/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=metric`
+  )
+    .then((res) => res.json())
+    .then((data) => {
+      const iconUrl = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+      document.getElementById("weather").innerHTML = `
+      <img src="${iconUrl}">
+      <p class="weather-temp">${Math.round(data.main.temp)} °C</p>
+      <p  class="weather-city">${data.name}</p>
+      `;
+    });
+});
